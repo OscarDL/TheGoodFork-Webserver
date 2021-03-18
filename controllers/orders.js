520edc31a188199a4a9862ef5e3f037bf336db4b
Oscar Di Lenarda <oscar@dilenarda.com>
2021-03-18 22:42:14 +0100

    Added delete order logic & route

--- controllers/orders.js
+++ controllers/orders.js
@@ -29,7 +29,7 @@ exports.createOrder = async (req, res, next) => {
       <h2>${user?.firstName || user?.email},</h2>
       <br/><h3>Your order has been placed successfully.</h3>
       <p>Our cooks will deliver your order as soon as it's ready.</p><br/>
-      <br/>For recall, here's your order details:
+      For recall, here's your order details:
       <ul>
         ${orderContent?.appetizer?.length > 0 ? `<li>Appetizers:
           <ul>
@@ -46,7 +46,7 @@ exports.createOrder = async (req, res, next) => {
             ${orderContent.dessert.map(dess => `<li>${dess}</li>`)}
           </ul>
         </li>` : ''}
-      </ul>
+      </ul><br/>
       <h4>Thank you for your support, we hope you enjoy your meal and comeback for future ones.</h4>
       <p>The Good Fork &copy; - 2021</p>
     `;
@@ -63,7 +63,7 @@ exports.createOrder = async (req, res, next) => {
       });
 
       sendEmail({email: user.email, subject: 'The Good Fork - Meal Order', content});
-      res.status(200).json({success: true, order});
+      return res.status(200).json({success: true, order});
 
     } catch (error) { return next(error); }
     
@@ -72,14 +72,21 @@ exports.createOrder = async (req, res, next) => {
 
 
 exports.editOrder = async (req, res, next) => {
+  let token;
   const {orderContent, price} = req.body;
 
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
+    token = req.headers.authorization.split(' ')[1];
+
+  if (!token)
+    return next(new ErrorResponse('Could not get your orders, please try again or sign out then in again.', 401));
+
+
+  if (!req.params.orderid || !req.body.orderContent)
+    return next(new ErrorResponse('Could not retrieve your order information.', 400));
+
+    
   try {
-
-    if (!req.params.orderid || !req.body.orderContent)
-      return next(new ErrorResponse('Could not retrieve your order information.', 400));
-      
-
     const order = await Order.findOne({_id: req.params.orderid});
 
     if (!order)
@@ -94,6 +101,32 @@ exports.editOrder = async (req, res, next) => {
       res.status(200).json({success: true, order});
 
     } else res.status(423).json({success: false, error: 'You cannot edit your order anymore.'});
+    
+  } catch (error) { return next(error); }
+};
+
+
+exports.deleteOrder = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
+    token = req.headers.authorization.split(' ')[1];
+
+  if (!token)
+    return next(new ErrorResponse('Could not get your orders, please try again or sign out then in again.', 401));
+
+
+  if (!req.params.orderid)
+    return next(new ErrorResponse('Could not retrieve your order information.', 400));
+
+    
+  try {
+    const order = await Order.deleteOne({_id: req.params.orderid});
+
+    if (!order)
+      return next(new ErrorResponse('Could not delete your order, please try again.', 404));
+
+    res.status(200).json({success: true});
     
   } catch (error) { return next(error); }
 };
--- routes/orders.js
+++ routes/orders.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const { createOrder, editOrder, getOrders, validateOrder } = require('../controllers/orders');
+const { createOrder, editOrder, getOrders, validateOrder, deleteOrder } = require('../controllers/orders');
 
 const router = express.Router();
 
 router.route('/').get(getOrders);
 router.route('/create').post(createOrder);
 router.route('/edit/:orderid').put(editOrder);
+router.route('/delete/:deleteid').delete(deleteOrder);
 router.route('/validate/:orderid').put(validateOrder);
 
 module.exports = router;
